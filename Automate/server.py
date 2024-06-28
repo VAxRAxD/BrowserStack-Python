a470ff85fbada08f2bda159c485e2f9e9a6ecb2a
VAxRAxD <varadp763@gmail.com>
2024-06-28 15:02:37 +0530

    Added more features

--- Automate/server.py
+++ Automate/server.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, make_response
+
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, BrowserStack!'
+    html_content = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BrowserStack</title>
+    </head>
+    <body>
+        <h1>Hello, BrowserStack</h1>
+        <h3>I am Automation Support Engineer</h3>
+        <h5>I solve real problems</h5>
+        <button class="link-button" onclick="location.href='https://github.com/VAxRAxD'">Go to Github</button>
+    </body>
+    </html>
+    """
+    return make_response(html_content)
+
 if __name__ == '__main__':
     app.run(debug=True)
